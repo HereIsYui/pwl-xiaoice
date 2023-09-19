71754eacd4428379cac3f6883e14e6da80c5978e
Yui <529247970@qq.com>
2023-09-19 19:57:49 +0800

    修改活动规则

--- src/Utils/xiaoIceLib/activity.ts
+++ src/Utils/xiaoIceLib/activity.ts
@@ -6,21 +6,21 @@ import FishPi, { FingerTo } from "fishpi";
 
 export const activityRuleList = [
   {
-    rule: /^国庆快乐$/,
+    rule: /摸鱼派祝大家国庆快乐/,
     func: async ({ data, IceNet, conf, fish }: RuleParams) => {
       let cb = await activityReward(data, IceNet, 1, 7, conf, fish);
       return cb;
     },
   },
   {
-    rule: /^中秋快乐$/,
+    rule: /摸鱼派祝大家中秋快乐/,
     func: async ({ data, IceNet, conf, fish }: RuleParams) => {
       let cb = await activityReward(data, IceNet, 1, 3, conf, fish);
       return cb;
     },
   },
   {
-    rule: /^欢庆双节$/,
+    rule: /摸鱼派祝大家欢庆双节/,
     func: async ({ data, IceNet, conf, fish }: RuleParams) => {
       let cb = await activityReward(data, IceNet, 1, 8, conf, fish);
       return cb;
@@ -36,11 +36,12 @@ async function activityReward(
   conf: any,
   fish: any
 ) {
-  let cb = `:tada:恭喜触发\`关键词${activityId}\` 奖励已到账,可在账户中查看~`;
+  let cb = ``;
   if (dayjs().isBefore("2023-09-27")) {
     cb = "活动还没有开始哦~";
     return cb;
   }
+  // 判断是否已经领取过
   let Auser = await IceNet.activityRecord.findOne({
     where: { userId: IceNet.UDetail.uId },
   });
@@ -48,6 +49,14 @@ async function activityReward(
     cb = `:jack_o_lantern:已经领取过啦,不可以重复领取哦`;
     return cb;
   }
+  // 口令查重
+  let isRepeat = await IceNet.activityRecord.findOne({
+    where: { content: data.msg },
+  });
+  if (isRepeat) {
+    return `:jack_o_lantern: 该口令已经被使用过啦~`;
+  }
+
   let activity_record = new ActivityRecordEntity();
   activity_record.userId = IceNet.UDetail.uId;
   activity_record.activityName = "2023国庆活动";
@@ -56,6 +65,6 @@ async function activityReward(
   IceNet.activityRecord.save(activity_record);
   await fish.chatroom.revoke(data.oId);
   FingerTo(conf.keys.item).editUserBag(data.user, "sysCheckinRemain", day);
-  cb = `:tada:恭喜触发\`关键词1\` 奖励已到账,可在账户中查看~`;
+  cb = `:tada:恭喜触发\`关键词${activityId}\` 奖励已到账,可在账户中查看~`;
   return cb;
 }
