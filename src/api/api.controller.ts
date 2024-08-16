d277c8f3fe64b519d7a3bb7bd117154f5c6f8b5c
Yui <44658840+HereIsYui@users.noreply.github.com>
2024-08-16 17:35:55 +0800

    修改查询参数

--- src/api/api.controller.ts
+++ src/api/api.controller.ts
@@ -1,22 +1,22 @@
-import { Controller, Get, UseGuards, Query } from '@nestjs/common';
-import { ApiService } from './api.service';
-import { CusGuardGuard } from '../cus-guard/cus-guard.guard';
+import { Controller, Get, UseGuards, Query } from "@nestjs/common";
+import { ApiService } from "./api.service";
+import { CusGuardGuard } from "../cus-guard/cus-guard.guard";
 
-@Controller('api')
+@Controller("api")
 export class ApiController {
-  constructor(private readonly apiService: ApiService) { }
+  constructor(private readonly apiService: ApiService) {}
 
-  @Get('GetUserIntimacy')
+  @Get("GetUserIntimacy")
   @UseGuards(CusGuardGuard)
   GetUserIntimacy(@Query() query): any {
-    if (!query.user) return { code: 1, msg: 'user是必填的' }
-    return this.apiService.GetUserIntimacy(query.user);
+    if (!query.uid) return { code: 1, msg: "uid是必填的" };
+    return this.apiService.GetUserIntimacy(query.uid);
   }
 
-  @Get('GetUserCreditScore')
+  @Get("GetUserCreditScore")
   @UseGuards(CusGuardGuard)
   GetUserCreditScore(@Query() query): any {
-    if (!query.user) return { code: 1, msg: 'user是必填的' }
-    return this.apiService.GetUserCreditScore(query.user);
+    if (!query.uid) return { code: 1, msg: "uid是必填的" };
+    return this.apiService.GetUserCreditScore(query.uid);
   }
 }
--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -1,27 +1,31 @@
-import { Injectable } from '@nestjs/common';
-import { User } from '../entities/user.entities'
-import { Credit } from '../entities/credit.entities';
-import { Repository } from 'typeorm'
-import { InjectRepository } from '@nestjs/typeorm'
-
+import { Injectable } from "@nestjs/common";
+import { User } from "../entities/user.entities";
+import { Credit } from "../entities/credit.entities";
+import { Repository } from "typeorm";
+import { InjectRepository } from "@nestjs/typeorm";
 
 @Injectable()
 export class ApiService {
-  constructor(@InjectRepository(User) private readonly user: Repository<User>,
-    @InjectRepository(Credit) private readonly credit: Repository<Credit>) { }
-  async GetUserIntimacy(user: string) {
+  constructor(
+    @InjectRepository(User) private readonly user: Repository<User>,
+    @InjectRepository(Credit) private readonly credit: Repository<Credit>
+  ) {}
+  async GetUserIntimacy(uid: string) {
     return {
       code: 0,
-      data: await this.user.findOne({ select: ['uId', 'user', 'intimacy'], where: { user } }),
-      msg: 'success'
-    }
+      data: await this.user.findOne({ select: ["uId", "user", "intimacy"], where: { uId: uid } }),
+      msg: "success",
+    };
   }
 
-  async GetUserCreditScore(user: string) {
+  async GetUserCreditScore(uid: string) {
     return {
       code: 0,
-      data: await this.credit.findOne({ select: ['uId', 'user', 'base_score', 'activity_score', 'reward_score', 'credit_score'], where: { user } }),
-      msg: 'success'
-    }
+      data: await this.credit.findOne({
+        select: ["uId", "user", "base_score", "activity_score", "reward_score", "credit_score"],
+        where: { uId: uid },
+      }),
+      msg: "success",
+    };
   }
 }
